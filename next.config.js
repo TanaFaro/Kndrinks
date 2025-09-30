b735af0e1712114b574d9e60e6ce4c22d67caa87
Ivana <ivanafarotto76@gmail.com>
2025-09-30 04:01:00 -0300

    Forzar redeploy: agregar configuraciÃ³n de imÃ¡genes en next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // Configuración para servir imágenes estáticas
+  images: {
+    unoptimized: true
+  }
+}
 
 module.exports = nextConfig
